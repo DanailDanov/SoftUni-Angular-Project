cf5b6ad17bde30d05a79a8d7fcfd4ad65e649d93
DanailDanov <114019922+DanailDanov@users.noreply.github.com>
2023-07-21 15:02:43 +0300

    creating footer component

--- E-Commerce-Store/src/app/core/core.module.ts
+++ E-Commerce-Store/src/app/core/core.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from '../material/material.module';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
   ],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule { }
