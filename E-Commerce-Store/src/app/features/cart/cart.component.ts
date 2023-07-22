9cbebc8c0345f4df3612d24cbb3052977a17ccba
DanailDanov <114019922+DanailDanov@users.noreply.github.com>
2023-07-22 18:50:17 +0300

    adding cart functionality

--- E-Commerce-Store/src/app/features/cart/cart.component.ts
+++ E-Commerce-Store/src/app/features/cart/cart.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
-import { Cart } from 'src/app/types/cart';
+import { Component, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/types/cart';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
-  cart: Cart = {items: []};
+export class CartComponent implements OnInit {
+  cart: Cart = {
+    items: [{
+      product: 'https://via.placeholder.com/150',
+      name: 'Athens',
+      price: 150,
+      quantity: 1,
+      id: 1,
+    },
+    {
+      product: 'https://via.placeholder.com/150',
+      name: 'Athens',
+      price: 350,
+      quantity: 1,
+      id: 2,
+    }]
+  };
+
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
+  ];
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items.map((item) => item.price * item.quantity)
+                .reduce((prev, current) => prev + current, 0);
+  }
 }
